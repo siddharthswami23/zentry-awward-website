85b2f4d740e2712e8465955e918eef0328e429e4
siddharthswami23 <siddharth.swami@walchandsangli.ac.in>
2025-07-23 19:09:04 +0530

    new pages added

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,15 @@
-import React from 'react';
+import React from "react";
+import Hero from "./components/Hero";
+import Navbar from "./components/Navbar";
 
 const App = () => {
   return (
-    <div className='bg-red-300 font-circular-web'>
-      app
-    </div>
+    <main className="relative min-h-screen w-screen overflow-x-hidden">
+      {/* <Navbar /> */}
+      <Hero />
+      <section className="relative z-10 h-dvh w-screen overflow-hidden rounded-lg bg-blue-75"></section>
+    </main>
   );
-}
+};
 
 export default App;
